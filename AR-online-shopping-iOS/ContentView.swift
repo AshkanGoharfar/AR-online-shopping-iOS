3596b34e68488620b1aaca23d2abde1efb6d6e7d
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-19 11:38:02 -0400

    Initial Commit

---
+++ AR-online-shopping-iOS/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  AR-online-shopping-iOS
+//
+//  Created by Ashkan Goharfar on 2022-03-19.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
