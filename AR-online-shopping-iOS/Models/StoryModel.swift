e9b4ff0698e31f9bf263354b503490e8a3942daf
Al-Rou <57326693+Al-Rou@users.noreply.github.com>
2022-04-20 18:37:39 -0400

    Update StoryModel.swift

--- AR-online-shopping-iOS/Models/StoryModel.swift
+++ AR-online-shopping-iOS/Models/StoryModel.swift
@@ -2,11 +2,11 @@
 //  StoryModel.swift
 //  AR-online-shopping-iOS
 //
-//  Created by Ashkan Goharfar on 2022-04-12.
+//  Created by Ali Roudak on 2022-04-12.
 //
 
 import Foundation
-
+//This struct is to support offers which appear on the home page of the app
 struct StoryModel: Identifiable,Hashable {
     
     // Mirror data and data types in firebase database
